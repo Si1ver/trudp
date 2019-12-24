2d1be398b323a87da677b2626142feeecf46f7f8
Anton Vasiliev <si1ver1502@gmail.com>
2019-12-24 13:30:25 +0300

    Add packet type.

--- include/trudppp/packet.hpp
+++ include/trudppp/packet.hpp
@@ -17,6 +17,55 @@ namespace trudppp {
         Ping = 4, ///< Ping packet.
         AckOnPing = 5, ///< Pong packet.
     };
+
+    /// Trudppp packet.
+    class Packet {
+    private:
+        /// The type of the packet.
+        PacketType type;
+
+        /// Channel number.
+        uint8_t channel_number;
+
+        /// Packet identifier.
+        uint32_t id;
+
+        /// Payload data transmitted with packet.
+        std::vector<uint8_t> data;
+
+        /// The moment of time when the packet was sent.
+        std::chrono::system_clock::time_point timestamp;
+
+    public:
+        Packet() : type(PacketType::Data), channel_number(0), id(0) {}
+
+        Packet(PacketType type, uint8_t channel_number, uint32_t id, std::vector<uint8_t> data,
+            std::chrono::system_clock::time_point timestamp)
+            : type(type), channel_number(channel_number), id(id), data(data), timestamp(timestamp) {
+        }
+
+        // TODO: copy constructors and assignment.
+
+        inline PacketType GetType() const { return type; }
+
+        inline uint8_t GetChannelNumber() const { return channel_number; }
+
+        inline uint32_t GetId() const { return id; }
+
+        inline const std::vector<uint8_t>& GetData() const { return data; }
+
+        inline std::chrono::system_clock::time_point GetTimestamp() const { return timestamp; }
+
+        inline void SetTimestamp(std::chrono::system_clock::time_point new_timestamp) {
+            using namespace std::chrono;
+            timestamp = time_point_cast<microseconds>(new_timestamp);
+        }
+
+        inline void SetTimestampToNow() {
+            using namespace std::chrono;
+            timestamp = time_point_cast<microseconds>(system_clock::now());
+        }
+    };
 } // namespace trudppp
 
 #endif
